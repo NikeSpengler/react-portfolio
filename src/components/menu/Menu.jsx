efd0579bb5bd29641d55a9307205975b0f4985a5
Nike <spengler.nike@gmail.com>
2022-02-07 13:40:32 +0100

    update

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,16 @@
+import "./menu.scss"
+
+export default function Menu() {
+  return(
+    <div className="menu">
+        <ul>
+            <li>
+                <a href="#about">About</a>
+                <a href="#works">Work</a>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </div>
+
+  ) 
+}
