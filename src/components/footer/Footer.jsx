ff737750b89fc1df53379b8a7f7248788eefc70f
Nike <spengler.nike@gmail.com>
2022-03-11 10:17:58 +0100

    update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./footer.scss"
+
+
+
+
+export default function Footer () {
+
+  return <div className="footer" id="footer">
+        <div className="wrapper">
+            <div className="footer-text">
+                <p>
+                Developed by <b>Nike Spengler.</b> Built with <b>React.</b> Styled with <b>SASS.</b>
+                </p>
+            </div>
+         </div> 
+    </div>
+}
